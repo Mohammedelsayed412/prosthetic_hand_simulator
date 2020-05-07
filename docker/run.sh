a16157813f533644efbebc12a8f95837b29e802a
meena-abdelmaseeh <meena.abdelmaseeh@gmail.com>
2020-05-07 01:18:19 +0000

    modified docker/run.sh to pull from dockerhub and mount the repo

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8080:8080 -p 7681:7681 ceslab/gzweb  
+docker run -it --rm -p 8080:8080 -p 7681:7681 -v $(pwd)/..:/data --name ros_gazebo_web  cairoceslab/prosthetic_hand_simulator:latest /bin/bash
