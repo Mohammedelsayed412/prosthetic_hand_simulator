38a27d5de080e3bb0f82036ff26418fcb31bf447
Merna-Atef <35666049+Merna-Atef@users.noreply.github.com>
2020-05-28 13:00:07 +0200

    launch file done

---
+++ sim_env_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ceslab/sim_env_ws/src
+# Build directory: /home/ceslab/sim_env_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("prosthetic_gazebo")
---
+++ sim_env_ws/build/prosthetic_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ceslab/sim_env_ws/src/prosthetic_gazebo
+# Build directory: /home/ceslab/sim_env_ws/build/prosthetic_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
