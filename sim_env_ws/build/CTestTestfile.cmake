881ccf69203931753872c15f61b84bb4fed1e258
Merna-Atef <35666049+Merna-Atef@users.noreply.github.com>
2020-05-28 12:57:15 +0200

    trying something

--- sim_env_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ceslab/sim_env_ws/src
-# Build directory: /home/ceslab/sim_env_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("prosthetic_gazebo")
