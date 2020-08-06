38deafe3c915fc1d340b53392fcc66bed6bf25e9
petemad <peteremad97@hotmail.com>
2020-08-06 20:53:13 +0200

    Control camera to send image to API upon request

--- sim_env_ws/src/prosthetic_gazebo/scripts/subscriber_image.py
+++ sim_env_ws/src/prosthetic_gazebo/scripts/subscriber_image.py
@@ -4,15 +4,15 @@ import rospy
 import sys
 import cv2 as cv
 from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
 class cvBridgeDemo():
     def __init__(self):
-        self.node_name = "saveing_image"
-
+        self.node_name = "saving_image"
         rospy.init_node(self.node_name)
-
+        
         # What we do during shutdown
         rospy.on_shutdown(self.cleanup)
 
@@ -21,24 +21,39 @@ class cvBridgeDemo():
 
         # Subscribe to the camera image topics and set the appropriate callbacks
         self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.image_callback)
+        self.trigger_sub = rospy.Subscriber("/teleop_key", String, self.flag_update)
+        self.old_flag = "quit"
+        self.flag = False
         rospy.loginfo("Waiting for image topics...")
-
+        
+    def flag_update(self, ros_key):
+        if self.old_flag != ros_key.data :
+            if ros_key.data == "start" :
+                self.flag = True
+                self.old_flag = ros_key.data
+            else : 
+                self.flag = False
+                self.old_flag = ros_key.data
+            rospy.loginfo(ros_key)   
+    
     def image_callback(self, ros_image):
         # Use cv_bridge() to convert the ROS image to OpenCV format
-        try:
-            rospy.loginfo("trying to convert ros image to cv2")
-            frame = self.bridge.imgmsg_to_cv2(ros_image, desired_encoding='passthrough')
-            rospy.loginfo("coversion done successfully")
-        except CvBridgeError, e:
-            rospy.loginfo("Exception found ! ! !")
-            print e
+        rospy.loginfo("In image callback")
+        if self.flag :
+            try:
+                rospy.loginfo("trying to convert ros image to cv2")
+                frame = self.bridge.imgmsg_to_cv2(ros_image, desired_encoding='passthrough')
+                rospy.loginfo("coversion done successfully")
+            except CvBridgeError, e:
+                rospy.loginfo("Exception found ! ! !")
+                print e
+            
+            # Saving the image
+            rospy.loginfo("saving image")
+            cv.imwrite('captured_img.jpg', frame)
+            rospy.loginfo("image saved successfully in your work space named captured_img.jpg")
+            self.flag = False
         
-        
-        # Saving the image
-        rospy.loginfo("saving image")
-        cv.imwrite('captured_img.jpg', frame)
-        rospy.loginfo("image saved successfully in your work space named captured_img.jpg")
-
 
     def cleanup(self):
         print "Shutting down vision node."
